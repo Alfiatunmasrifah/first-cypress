06a5bb1dc4f3c3b21f47457869490a2390496a73
Alfiatunmasrifah <alfiatunmasrifah0@gmail.com>
2025-09-08 15:03:19 +0700

    add-logout user

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,8 +1,9 @@
-describe('Login launch', () => {
+describe('Logout launch', () => {
     it ('launch app', () =>{
          cy.visit('https://parabank.parasoft.com/parabank/index.htm')
          cy.get("input[name='username']").type('Kei')
          cy.get("input[name='password']").type('kei123')
          cy.get(':nth-child(5) > .button').click()
+         cy.get('#leftPanel > ul > :nth-child(8) > a').click()
     })
 })
